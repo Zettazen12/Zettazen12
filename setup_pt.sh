bca5bebd3b96f9da790976d0381e5b57323b33e4
Zettazen12 <165353439+Zettazen12@users.noreply.github.com>
2024-04-16 11:17:38 +0700

    Update setup_pt.sh

--- setup_pt.sh
+++ setup_pt.sh
@@ -5,8 +5,8 @@ nc="\033[0m"
 clear
 REPO="https://raw.githubusercontentcom/Zettazen12/Zettazen12/main"
 # Ambil script & install & hapus file instalasi
-wget -q https://${REPO}/zetta.sh
-mv zetta.sh /usr/local/sbin/pt
+wget -q ${REPO}/pt
+mv pt /usr/local/sbin/
 chmod +x /usr/local/sbin/*
 clear
 echo -e "${biru}Silahkan ketik ${hijau}pt ${biru}untuk mencobanya${nc}"
